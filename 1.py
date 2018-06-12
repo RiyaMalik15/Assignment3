8901337a703ff1316a189103a18ebee7c29ee916
RiyaMalik15 <riyamalik608@gmail.com>
2018-06-12 23:27:57 +0530

    first change

---
+++ 1.py
@@ -0,0 +1,51 @@
+print("Question1")
+a=input("Enter first element")
+b=input("Enter second element")
+c=input("Enter third eleement")
+d=input("Enter fourth element")
+list1=[a,b,c,d]
+print(list1)
+print('*'*10)
+print('\n')
+print("Question2")
+list2=['google','apple','facebok','microsoft','tesla']
+list1=list1+list2
+print(list1)
+print('*'*10)
+print('\n')
+print("Question3")
+print(list1.count('apple'))
+print(list1.count(a))
+print('*'*10)
+print('\n')
+print("Question4")
+list3=['5','3','8','1','2']
+list3.sort()
+print(list3)
+print('*'*10)
+print('\n')
+print("Question5")
+list4=['1','2','4','5','7','9']
+list5=['3','6','8']
+list6=list4+list5
+list6.sort()
+print(list6)
+print('*'*10)
+print('\n')
+print("Question6")
+stack=['Anshul','Gauri','Meenal']
+stack.append('Riya')
+stack.append('Manoj')
+print(stack)
+stack.pop()
+print(stack)
+queue=['Shweta','Manoj','Raman']
+print(queue)
+queue.append('Riya')
+queue.append('Gauri')
+print(queue)
+queue.pop(0)
+print(queue)
+queue.pop(0)
+print(queue)
+print('*'*10)
